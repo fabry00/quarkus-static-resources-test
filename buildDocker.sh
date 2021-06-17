6b0a2f2000d098de795b7963f00738c2b6aca76b
fabry00 <fabry_00@hotmail.com>
2021-06-17 17:43:13 +0200

    WIP

--- buildDocker.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-set -x
-docker --version
-
-#docker build --no-cache -f src/main/docker/Dockerfile.multistage -t quarkus-quickstart/rest-client-quickstart .
-docker build -f src/main/docker/Dockerfile.multistage -t quarkus-quickstart/rest-resources-static-resources .
--- localBuild.sh
+++
@@ -1,4 +0,0 @@
-export JAVA_HOME=/Library/Java/JavaVirtualMachines/jdk1.8.0_121.jdk/Contents/Home
-export PATH="/Library/Java/JavaVirtualMachines/jdk1.8.0_121.jdk/Contents/Home/bin:$PATH"
-
-./mvnw clean quarkus:dev
